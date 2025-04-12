d4c063174c63aa296575a4087d896afb93721821
Pranjal Srivastava <pranu.sri1903@gmail.com>
2025-04-13 02:08:29 +0530

    Attendee

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class Attendee {
+public:
+    string name;
+    string contact;
+    bool rsvp;
+    bool hasGuest;
+    string guestName;
+    bool needsSecurity;
+public:
+    Attendee(string name, string contact, bool rsvp,
+             bool hasGuest = false, string guestName = "", bool needsSecurity = false)
+        : name(name), contact(contact), rsvp(rsvp), hasGuest(hasGuest),
+          guestName(guestName), needsSecurity(needsSecurity) {}
+    void display() {
+        cout << "\nAttendee Details:\n";
+        cout << "Name: " << name << endl;
+        cout << "Contact: " << contact << endl;
+        cout << "RSVP: " << (rsvp ? "Yes" : "No") << endl;
+        if (hasGuest) {
+            cout << "Guest Attending: Yes" << endl;
+            cout << "Guest Name: " << guestName << endl;
+            cout << "Security Needed for Guest: " << (needsSecurity ? "Yes" : "No") << endl;
+        } else {
+            cout << "Guest Attending: No" << endl;
+        }
+    }
+};
